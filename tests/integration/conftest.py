0eecb5281fbac82c95f85c2fe2adeb2c660aa8a7
Clifford Duke <clifduke@amazon.com>
2025-04-24 04:33:54 +0000

    Fixed testing configuration errors

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -6,7 +6,8 @@ import os
 import pytest
 import time
 
-APPLICATION_STACK_NAME = os.getenv('ENV_STACK_NAME', None)
+APPLICATION_STACK_NAME = os.getenv('ENV_STACK_NAME', 'ws-serverless-patterns-dev')
+boto3.setup_default_session(region_name='us-west-2')
 globalConfig = {}
 
 
